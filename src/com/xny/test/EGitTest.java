d5da5698f985cb1cc52587cbd6b01640654c9140
qqq-creater-bi <qqq_creater@163.com>
2020-04-22 20:39:20 +0800

    fdfd

--- src/com/xny/test/EGitTest.java
+++ src/com/xny/test/EGitTest.java
@@ -4,6 +4,6 @@ public class EGitTest {
 	public static void main(String[] args) {
 		System.out.println("aaaaaa");
 		System.out.println("AAAAAA");
-		System.out.println("dasdsa");
+		System.out.println("dasfddsa");
 	}
 }
