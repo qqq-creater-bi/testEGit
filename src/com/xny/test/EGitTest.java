7af12669cfea57539309a4bf263347c05a0639b3
qqq-creater-bi <qqq_creater@163.com>
2020-04-22 20:41:10 +0800

    f

--- src/com/xny/test/EGitTest.java
+++ src/com/xny/test/EGitTest.java
@@ -4,5 +4,8 @@ public class EGitTest {
 	public static void main(String[] args) {
 		System.out.println("aaaaaa");
 		System.out.println("AAAAAA");
+		System.out.println("AAAAAA");
+		System.out.println("AAAAAA");
+		System.out.println("AAAAAA");
 	}
 }
